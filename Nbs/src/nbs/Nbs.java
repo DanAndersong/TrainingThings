91894e5f3e40ab9cb2e5853a9cdd940b3044d91f
User <user@Users-MacBook-Pro.local>
2022-01-05 22:23:31 +0300

    new project + number systems

---
+++ Nbs/src/nbs/Nbs.java
@@ -0,0 +1,13 @@
+package nbs;
+
+import static java.lang.System.out;
+
+public class Nbs {
+
+ 
+    public static void main(String[] args) {
+        NumberSystems.task();
+        
+    };
+    
+}
