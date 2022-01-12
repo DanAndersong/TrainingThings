4de6f017b7dbf32ff84110578c25d7d8d467b48f
User <user@Users-MacBook-Pro.local>
2022-01-12 21:56:01 +0300

    test

--- Nbs/src/ten/p4/p5/E.java
+++ Nbs/src/ten/p4/p5/E.java
@@ -8,11 +8,16 @@ public class E {
     int e = 0;
     static boolean es = false;
 
+    public static boolean isEs() {
+        return es;
+    }
+
     public E() {
     }
 
     public int getE() {
         return e;
     }
+    
     
 }
