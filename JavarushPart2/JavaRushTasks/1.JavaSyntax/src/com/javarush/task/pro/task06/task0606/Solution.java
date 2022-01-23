7fdd9997e1c2ad7fbb5b5005c77430f9e4700006
User <mr.den.andersong@gmail.com>
2022-01-23 22:48:22 +0300

    add tasks

---
+++ JavarushPart2/JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(int i){
+
+    }
+    public static void universalMethod(int[] k){
+
+    }
+    public static void universalMethod(String s){
+
+    }
+    public static void universalMethod(int saf, String bubble){
+
+    }
+    public static void universalMethod(Solution s){
+
+    }
+    public static void universalMethod(int i, Solution s){
+
+    }
+    public static void universalMethod(double d){
+
+    }
+    public static void universalMethod(String val, Double d){
+
+    }
+    public static void universalMethod(Double d){
+
+    }
+    public static void universalMethod(Byte b){
+
+    }
+
+    //напишите тут ваш код
+}
