d624b4c243224b36cf3312f9b96b5bcdf319528a
User <mr.den.andersong@gmail.com>
2022-01-26 23:36:34 +0300

    JR add new tasks

---
+++ JavarushPart2/JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int i;
+    float f;
+    double d;
+    long l;
+    byte b;
+    short s;
+    char c;
+    boolean bool;
+}
