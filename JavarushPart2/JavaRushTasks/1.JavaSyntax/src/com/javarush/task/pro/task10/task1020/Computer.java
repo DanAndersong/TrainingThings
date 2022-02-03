f75d21fe3fc0c50a4d02c575719476e096e90216
User <mr.den.andersong@gmail.com>
2022-02-03 22:21:46 +0300

    new tasks

--- JavarushPart2/JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
+++ JavarushPart2/JavaRushTasks/1.JavaSyntax/src/com/javarush/task/pro/task10/task1020/Computer.java
@@ -1,10 +1,22 @@
 package com.javarush.task.pro.task10.task1020;
 
+import java.util.Locale;
+
 public class Computer {
     public Computer() {
         new Keyboard();
         new Monitor();
         new Mouse();
         new SystemUnit();
+
+    }
+
+    public static void main(String[] args) {
+        String c = "count";
+        String b = "count";
+        String d = "1count";
+        String str = new String("count");
+        String sstr = new String("count");
+        System.out.println(str == c);
     }
 }
