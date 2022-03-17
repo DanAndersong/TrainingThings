f17b158e7e7eae68c2db06e6783a2252752bd89b
User <mr.den.andersong@gmail.com>
2022-03-17 22:24:56 +0300

    changed id quantity element to message

--- android/MyCoffee/app/src/main/java/com/example/mycoffee/MainActivity.java
+++ android/MyCoffee/app/src/main/java/com/example/mycoffee/MainActivity.java
@@ -10,7 +10,9 @@ import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
     private int price = 5;
-    private int coffeeCount = 0;
+    private int quantity = 0;
+    String message;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +21,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        displayPrice();
+        int totalPrice = price * quantity;
+        message = String.format(
+                "Name:%s\n" +
+                "Quantity: %s\n" +
+                "Total: %s\n" +
+                "Thank you!",
+                name, quantity, totalPrice);
+        displayMessage();
     }
 
     public void incrementCoffeeNumber(View view) {
-        coffeeCount++;
+        quantity++;
         displayPrice();
         displayQuantity();
     }
 
     public void decrementCoffeeNumber(View view) {
-        if (coffeeCount > 0) {
-            coffeeCount--;
+        if (quantity > 0) {
+            quantity--;
             displayPrice();
             displayQuantity();
         }
@@ -38,11 +47,17 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayQuantity() {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
-        quantityTextView.setText(String.valueOf(coffeeCount));
+        quantityTextView.setText(String.valueOf(quantity));
     }
 
     private void displayPrice() {
-        TextView priceTextView = findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(price * coffeeCount));
+        TextView priceTextView = findViewById(R.id.order_summary_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(price * quantity));
+    }
+
+    private void displayMessage() {
+        TextView messageView = findViewById(R.id.order_summary_text_view);
+        messageView.setText(message);
+        findViewById(R.id.quantity_layout_view).setVisibility(View.INVISIBLE);
     }
 }
